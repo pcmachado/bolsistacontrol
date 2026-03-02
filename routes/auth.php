0f86b34d1c709d87ab2113f6bc0811266675f368
Giovane da Silva Bertol <giovane.bertol@ifrs.edu.br>
2026-03-02 14:15:45 -0300

    Refatorar: comentar a rota 'reset-password' para evitar uso acidental.

--- routes/auth.php
+++ routes/auth.php
@@ -28,8 +28,8 @@ Route::middleware('guest')->group(function () {
     // Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])
     //     ->name('password.email');
 
-    Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])
-        ->name('password.reset');
+    // Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])
+    //     ->name('password.reset');
 
     Route::post('reset-password', [NewPasswordController::class, 'store'])
         ->name('password.store');
